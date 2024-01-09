29e97ec5b76ea96039be8d290e18467992714eb6
jeong202 <144635774+L-jy16@users.noreply.github.com>
2024-01-09 14:29:22 +0900

    2023.01.09

---
+++ src/components/reple/RepleAreas.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import RepleList from './RepleList'
+import RepleWrite from './RepleWrite'
+
+const RepleAreas = () => {
+  return (
+    <>
+      <RepleList />
+      <RepleWrite />
+    </>
+  )
+}
+
+export default RepleAreas
---
+++ src/components/work/Youtube.jsx
@@ -0,0 +1,69 @@
+import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { gsap } from "gsap";
+import RepleAreas from '../reple/RepleAreas';
+
+
+const Youtube = () => {
+    // 화면 전환 효과
+    useEffect(() => {
+
+        gsap.set(".work_img_wrap", { opacity: 0, x: "-100%" });
+        gsap.set(".work__title, .center_text, .comment, .desc, .work_back_button, .work_btn", { opacity: 0 });
+
+        setTimeout(() => {
+            const tl = gsap.timeline();
+
+
+            tl.to(".work_img_wrap", { opacity: 1, x: 0, duration: 0.41 })
+            tl.to(".work__title", { opacity: 1, duration: 0.41 })
+            tl.to(".center_text", { opacity: 1, duration: 0.41 })
+            tl.to(".desc", { opacity: 1, duration: 0.41 })
+            tl.to(".comment", { opacity: 1, duration: 0.41 })
+            tl.to(".work_back_button", { opacity: 1, duration: 0.41 })
+            tl.to(".work_btn", { opacity: 1, duration: 0.41 })
+        })
+    }, [])
+
+    return (
+        <div className='work_Detail'>
+            <div className="work__title">
+                <h2>portfolio</h2>
+                <span>lee ji young</span>
+            </div>
+            {/* work__title */}
+
+            <div className="work_center left">
+                <span className='center_text'>youtube <p>site</p></span>
+                <div className="work_img_wrap youtube">
+                </div>
+                <div className="desc">
+                    <span>
+                        REACT를 활용하여 구현한 애니메이션 관련 유튜브 영상을 모은 사이트입니다.
+                        Youtube API를 활용하여 동적으로 실시간 유튜브 콘텐츠를 검색하고 동영상을 불러오기 기능을 제공하였습니다.
+                    </span>
+
+                </div>
+            </div>
+            {/* work_center */}
+
+            <div className="work_footer">
+                <div className="link_button">
+                    <Link to='/home' className="work_back_button">
+                        <span>Main site</span>
+                    </Link>
+                </div>
+                <div className="work_btn">
+                    <Link to="http://ljy16.dothome.co.kr/javascript/slider/slider01.html">view</Link>
+                    <Link to="https://github.com/L-jy16/webs2024/tree/main/javascript/slider">code</Link>
+                </div>
+            </div>
+            {/* work_footer */}
+
+            {/* comment */}
+            <RepleAreas />
+        </div>
+    )
+}
+
+export default Youtube
